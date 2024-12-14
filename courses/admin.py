693046fcff82041d8272a52c755cae67c502ecb6
measivs <meako.sivsivadze@gmail.com>
2024-12-14 18:42:46 +0400

    Add admin registration for Course-related models
    Implemented Django admin registration for the Category, Course, Enrollment, and Tag models to enable their management via the admin interface. This update improves the usability and accessibility of these models for administrative purposes.

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from courses.models import Course, Category, Enrollment, Tag
+
+
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
