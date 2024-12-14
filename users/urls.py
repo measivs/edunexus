d7a58acdd1f574c70e194d74caffb43bb8a51471
measivs <meako.sivsivadze@gmail.com>
2024-12-14 23:13:47 +0400

    Add email verification endpoint to users app
    Introduced a new URL pattern for email verification by adding `VerifyEmailCodeView`. This enhances user onboarding by allowing email verification as part of the registration process. Updated `users/urls.py` to reflect this new functionality.

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import LoginUserView, RegisterUserView, ProfileUserView
+from .views import LoginUserView, RegisterUserView, ProfileUserView, VerifyEmailCodeView
 from rest_framework.routers import DefaultRouter
 
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
+    path('verify/', VerifyEmailCodeView.as_view(), name='verify'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('profile/', ProfileUserView.as_view(), name='profile'),
 ]
