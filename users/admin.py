ca03c212fdecbd437886f20ecccf6a4c246d6e4d
measivs <meako.sivsivadze@gmail.com>
2024-12-14 17:59:55 +0400

    Add admin interface for AccountBalance model
    Registered the AccountBalance model in the Django admin site by creating a basic AccountBalanceAdmin class. This allows admin users to manage AccountBalance entries directly from the admin interface.

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.db.models import Model
 
-from .models import CustomUser
+from .models import CustomUser, AccountBalance
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(AccountBalance)
+class AccountBalanceAdmin(admin.ModelAdmin):
     pass
