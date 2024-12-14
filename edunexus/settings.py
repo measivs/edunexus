fb3e34451a097f811b457bc79d99fea5c3dae0b8
measivs <meako.sivsivadze@gmail.com>
2024-12-14 23:13:16 +0400

    Add caching and email configuration to settings
    Configured a local in-memory cache using Django's `LocMemCache` backend. Added SMTP email settings with environment variables for secure credential management. These changes enhance app performance and enable email functionality.

--- edunexus/settings.py
+++ edunexus/settings.py
@@ -172,3 +172,18 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=6),
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
