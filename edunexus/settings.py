c220e4fa3d41d462886f5e548197ae789821f862
measivs <meako.sivsivadze@gmail.com>
2024-12-15 18:12:08 +0400

    Set OpenAPI schema class in REST framework settings
    Added `DEFAULT_SCHEMA_CLASS` to enable automatic schema generation using OpenAPI. This change enhances API documentation capabilities and improves developer experience.

--- edunexus/settings.py
+++ edunexus/settings.py
@@ -62,6 +62,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.openapi.AutoSchema',
 }
 
 
