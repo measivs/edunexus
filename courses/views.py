58128970e63d9e1248eee0fa94f8f2178c982cac
measivs <meako.sivsivadze@gmail.com>
2024-12-15 21:48:16 +0400

    Add CourseViewSet with permissions and filtering
    Introduce a CourseViewSet leveraging Django Rest Framework. This includes filtering, custom permissions for instructors, and context-aware serializers to better handle course data.

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Course
+from .serializers import CourseSerializer
+from .filters import CourseFilter
+from .permissions import IsInstructor
 
-# Create your views here.
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = CourseFilter
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsAuthenticated, IsInstructor]
+        else:
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
+
+    def get_serializer_context(self):
+        return {'request': self.request}
