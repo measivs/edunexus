a5428e2125067c79b752d1c68ec9f9243f0e1c7e
measivs <meako.sivsivadze@gmail.com>
2024-12-16 00:52:54 +0400

    Add placeholder EnrollmentSerializer class to serializers.py
    Introduced a new `EnrollmentSerializer` class as a placeholder for future implementation. This prepares the codebase for handling enrollment-related serialization requirements.

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,13 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status, permissions
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from .models import Course
-from .serializers import CourseSerializer
-from .filters import CourseFilter
+from .models import Course, Enrollment
+from orders.models import Order
 from .permissions import IsInstructor
+from .serializers import CourseSerializer, EnrollmentSerializer
+from .filters import CourseFilter
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
@@ -14,10 +17,27 @@ class CourseViewSet(ModelViewSet):
 
     def get_permissions(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
-            self.permission_classes = [IsAuthenticated, IsInstructor]
+            self.permission_classes = [permissions.IsAuthenticated, IsInstructor]
+        elif self.action == 'enroll':
+            self.permission_classes = [permissions.IsAuthenticated]
         else:
-            self.permission_classes = [AllowAny]
+            self.permission_classes = [permissions.AllowAny]
         return super().get_permissions()
 
     def get_serializer_context(self):
         return {'request': self.request}
+
+    @action(detail=True, methods=['post'], permission_classes=[permissions.IsAuthenticated], serializer_class=EnrollmentSerializer)
+    def enroll(self, request, pk=None):
+        course = self.get_object()
+        user = request.user
+
+        if not Order.objects.filter(user=user, course=course, status='completed').exists():
+            return Response({'status': 'order not found or not completed'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if Enrollment.objects.filter(user=user, course=course).exists():
+            return Response({'status': 'already enrolled'}, status=status.HTTP_400_BAD_REQUEST)
+
+        Enrollment.objects.create(user=user, course=course)
+
+        return Response({'status': 'user enrolled'}, status=status.HTTP_200_OK)
