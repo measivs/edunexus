f49bf2691303f6fbb92b8f634bc5f8a5885cb9ad
measivs <meako.sivsivadze@gmail.com>
2024-12-16 23:49:22 +0400

    Remove unused models from course admin registration
    Cleaned up the admin configuration by unregistering `Category` and `Tag` models, as they are no longer needed. This reduces clutter and ensures the admin interface focuses on relevant models only.

--- courses/admin.py
+++ courses/admin.py
@@ -1,22 +1,13 @@
 from django.contrib import admin
 
-from courses.models import Course, Category, Enrollment, Tag
+from courses.models import Course, Enrollment
 
-
-# Register your models here.
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    pass
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Enrollment)
 class EnrollmentAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
     pass
