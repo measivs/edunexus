6206d47deca71177aad2bef5eae8d0411d0a7c89
measivs <meako.sivsivadze@gmail.com>
2024-12-16 23:49:59 +0400

    Update URL routing to include nested lessons endpoint
    Refactored the course URLs to use a nested router for lessons under courses. This enables better organization and access to lesson resources tied to specific courses. The changes improve the URL structure and follow best practices for RESTful design.

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,14 @@
-from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import CourseViewSet
-
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedSimpleRouter
+from .views import CourseViewSet, LessonViewSet
 router = DefaultRouter()
-router.register(r'', CourseViewSet, basename='course')
+router.register(r'courses', CourseViewSet, basename='course')
+
+courses_router = NestedSimpleRouter(router, r'courses', lookup='course')
+courses_router.register(r'lessons', LessonViewSet, basename='lesson')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(courses_router.urls)),
 ]
