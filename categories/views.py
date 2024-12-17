219bd71751c5f02c891b2977f0a8ac3065da7a81
measivs <meako.sivsivadze@gmail.com>
2024-12-17 19:33:23 +0400

    Add CategoryViewSet for category management
    Introduce a new CategoryViewSet using DRF ModelViewSet to handle category management, including list, retrieve, create, update, and delete actions. Implement search and filtering by category name and define permissions for different actions.

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAdminUser, AllowAny
+from django_filters.rest_framework import DjangoFilterBackend
+from categories.models import Category
+from categories.serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    A ModelViewSet for managing categories.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['name']
+    filterset_fields = ['name']
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [AllowAny()]
+        return [IsAdminUser()]
