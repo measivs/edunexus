d3203a4dbae8ccd58fd88074b605e53fad01cf48
measivs <meako.sivsivadze@gmail.com>
2024-12-18 00:33:13 +0400

    Add reviews endpoint to courses nested router
    Integrated a reviews endpoint into the courses nested router by registering the ReviewViewSet. This enables handling review-related operations under the courses API structure.

--- courses/urls.py
+++ courses/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedSimpleRouter
+
+from reviews.views import ReviewViewSet
 from .views import CourseViewSet, LessonViewSet
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='course')
 
 courses_router = NestedSimpleRouter(router, r'courses', lookup='course')
 courses_router.register(r'lessons', LessonViewSet, basename='lesson')
+courses_router.register(r'reviews', ReviewViewSet, basename='review')
 
 urlpatterns = [
     path('', include(router.urls)),
