25ba02e5fbab680030644cb3d17f187c9344e8fb
measivs <meako.sivsivadze@gmail.com>
2024-12-18 00:31:29 +0400

    Add IsCourseOwner permission class
    Introduce a new permission class, IsCourseOwner, to check if the user is the owner of a course. This ensures proper access control by allowing safe methods for all users and edit actions only for the instructor of the course.

--- courses/permissions.py
+++ courses/permissions.py
@@ -3,3 +3,10 @@ from rest_framework.permissions import BasePermission
 class IsInstructor(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'instructor'
+
+
+class IsCourseOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return obj.instructor == request.user
