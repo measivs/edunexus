9438372d61a0a50388d9cfcff4e09751bc8c5882
measivs <meako.sivsivadze@gmail.com>
2024-12-18 00:33:23 +0400

    Disable unused reviews URL import
    Commented out the inclusion of the `reviews` app URLs in the `edunexus` project. This change helps clean up and streamline the URL configuration by disabling unused routes.

--- edunexus/urls.py
+++ edunexus/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     # path('orders/', include('orders.urls')),
-    # path('reviews/', include('reviews.urls')),
     path('', include('categories.urls')),
     path('', include('courses.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
