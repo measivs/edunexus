3cb11ee7bf95846019a5876c67f1583bc89ff7a7
measivs <meako.sivsivadze@gmail.com>
2024-12-19 19:09:26 +0400

    Add route for retrieving user balance
    Added a new `GetBalanceView` to the `users/urls.py` file, allowing users to fetch their account balance via the '/balance/' endpoint. This improves API functionality by providing access to balance data directly.

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import LoginUserView, RegisterUserView, ProfileUserView, VerifyEmailCodeView, AddBalanceView
+from .views import LoginUserView, RegisterUserView, ProfileUserView, VerifyEmailCodeView, AddBalanceView, GetBalanceView
 from rest_framework.routers import DefaultRouter
 
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('verify/', VerifyEmailCodeView.as_view(), name='verify'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('profile/', ProfileUserView.as_view(), name='profile'),
+    path('balance/', GetBalanceView.as_view(), name='get-balance'),
     path('balance/add/', AddBalanceView.as_view(), name='balance'),
 ]
