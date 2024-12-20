5d2229cfb13d01da5cb22b22536591731fb4d3d4
measivs <meako.sivsivadze@gmail.com>
2024-12-20 16:07:55 +0400

    Add password reset endpoints to user URLs
    Added `PasswordResetRequestView` and `PasswordResetConfirmView` to the user URL patterns. This enhances the authentication system by enabling users to request password resets and confirm them.

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path, include
-from .views import LoginUserView, RegisterUserView, ProfileUserView, VerifyEmailCodeView, AddBalanceView, GetBalanceView
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+from .views import LoginUserView, RegisterUserView, ProfileUserView, VerifyEmailCodeView, AddBalanceView, \
+    GetBalanceView, PasswordResetRequestView, PasswordResetConfirmView
 
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
+    path('password_reset/', PasswordResetRequestView.as_view(), name='password_reset_request'),
+    path('password_reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('verify/', VerifyEmailCodeView.as_view(), name='verify'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('profile/', ProfileUserView.as_view(), name='profile'),
