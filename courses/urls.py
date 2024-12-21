330b28ceb6f6da34c4c37eadbdca5d618ba05b7f
measivs <meako.sivsivadze@gmail.com>
2024-12-22 02:55:13 +0400

    Add URL pattern for PopularCoursesView
    Added a new route to expose the PopularCoursesView at 'courses/popular-courses/'. This enables clients to retrieve popular courses via a dedicated endpoint. Updated imports in `courses/urls.py` to include PopularCoursesView.

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedSimpleRouter
 
 from reviews.views import ReviewViewSet
-from .views import CourseViewSet, LessonViewSet
+from .views import CourseViewSet, LessonViewSet, PopularCoursesView
+
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='course')
 
@@ -12,6 +13,7 @@ courses_router.register(r'lessons', LessonViewSet, basename='lesson')
 courses_router.register(r'reviews', ReviewViewSet, basename='review')
 
 urlpatterns = [
+    path('courses/popular-courses/', PopularCoursesView.as_view(), name='popular-courses'),
     path('', include(router.urls)),
     path('', include(courses_router.urls)),
 ]
