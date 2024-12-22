6a44bacc362d672abdf0324effaefcd73140b6a1
measivs <meako.sivsivadze@gmail.com>
2024-12-22 22:29:03 +0400

    Add default account balance creation for new users
    Ensure new users receive an initial account balance of 50 as a bonus. This is implemented via a signal triggered upon user creation, streamlining the onboarding process.

--- users/signals.py
+++ users/signals.py
@@ -4,6 +4,16 @@ from .models import CustomUser, AccountBalance
 
 @receiver(post_save, sender=CustomUser)
 def create_account_balance(sender, instance, created, **kwargs):
+    """
+        Signal triggered upon user creation.
+
+        Automatically creates an initial account balance for new users with a default value of 50 as a bonus.
+
+        Args:
+            sender: The model class (CustomUser).
+            instance: The instance of the newly-created user.
+            created: A boolean indicating whether the user was newly created.
+    """
     if created:
         account_balance = AccountBalance.objects.create(user=instance, balance=50)
         account_balance.save()
