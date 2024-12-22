5cce9cf68477414f72af1050fa98c36e0a8e5623
measivs <meako.sivsivadze@gmail.com>
2024-12-22 22:28:53 +0400

    Set default pagination and page size in DRF configuration
    Added PageNumberPagination as the default pagination class with a page size of 10 in Django REST Framework settings. This ensures consistent pagination for API responses across the application.

--- edunexus/settings.py
+++ edunexus/settings.py
@@ -68,6 +68,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.openapi.AutoSchema',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
