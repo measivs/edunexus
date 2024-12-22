c29ffb48b9499b39b7c98d6ac5824b84f152316c
measivs <meako.sivsivadze@gmail.com>
2024-12-22 22:29:38 +0400

    Refactor ReviewViewSet with enhanced docstring details
    Updated the `ReviewViewSet` class to include detailed docstrings, providing clarity on its functionality, permissions, and available methods. This improves code readability and helps developers understand the viewset's purpose and usage.

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,31 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Review, Course
 from .permissions import IsReviewOwner
 from .serializers import ReviewSerializer
 
 class ReviewViewSet(viewsets.ModelViewSet):
+    """
+        A viewset for managing course reviews.
+
+        Features:
+            - Supports Create, Read (List/Retrieve), Update, and Delete actions for reviews.
+            - Filters the queryset based on the course's
+              primary key (`course_pk`) provided in the URL.
+            - Ensures only authenticated users can create/update reviews
+              and only review owners can modify or delete their reviews.
+
+        Permissions:
+            - `IsAuthenticatedOrReadOnly`: Allows authenticated users
+               to create or modify reviews and unauthenticated users to only view reviews.
+            - `IsReviewOwner`: Restricts modification or deletion to the owner of the review.
+
+        Methods:
+            - `get_queryset`: Filters reviews for the specific
+               course based on `course_pk` provided in the URL.
+            - `perform_create`: Handles review creation, linking it to
+               the course and the user who created the review.
+    """
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsReviewOwner]
