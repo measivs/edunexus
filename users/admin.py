2e775f40debc6ba8986b73538374681a52f93e2e
measivs <meako.sivsivadze@gmail.com>
2024-12-23 21:18:33 +0400

    Enhance admin interface for CustomUser and AccountBalance
    Updated `CustomUserAdmin` with more fields in `list_display`, added filters, search, ordering, and editable fields to improve usability. Enhanced `AccountBalanceAdmin` to display user and balance, added search functionality, and enabled ordering by balance. These changes improve data visibility and management in the admin panel.

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from django.db.models import Model
-
 from .models import CustomUser, AccountBalance
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'is_verified')
+    list_display = ('id', 'username', 'email', 'role', 'is_verified', 'date_joined')
+    list_filter = ('role', 'is_verified', 'date_joined')
+    search_fields = ('username', 'email', 'role')
+    ordering = ('date_joined',)
+    list_editable = ('role', 'is_verified')
 
 @admin.register(AccountBalance)
 class AccountBalanceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'user', 'balance')
+    search_fields = ('user__username',)
+    ordering = ('balance',)
