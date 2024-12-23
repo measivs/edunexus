afc3d1afd2155281138a416259a530eb268f6219
measivs <meako.sivsivadze@gmail.com>
2024-12-23 21:18:21 +0400

    Enhance admin customization for Order and Coupon models
    Improved the admin interface by adding filters, search fields, and editable options for Order and Coupon models. Removed outdated actions to streamline the codebase and enhance usability. These changes aim to facilitate better management and accessibility in the admin panel.

--- orders/admin.py
+++ orders/admin.py
@@ -1,24 +1,18 @@
 from django.contrib import admin
 from orders.models import Order, Coupon
 
-# @admin.action(description='Mark selected orders as Completed')
-# def mark_as_completed(modeladmin, request, queryset):
-#     for order in queryset:
-#         if order.status == 'created':
-#             order.mark_completed()
-#
-#
-# @admin.action(description='Mark selected orders as Cancelled')
-# def mark_as_cancelled(modeladmin, request, queryset):
-#     for order in queryset:
-#         if order.status == 'created':
-#             order.mark_cancelled()
-
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'course', 'amount', 'status', 'created_at')
-
+    list_filter = ('status', 'created_at')
+    search_fields = ('user__username', 'course__title', 'status')
+    ordering = ('created_at',)
+    list_editable = ('status',)
 
 @admin.register(Coupon)
 class CouponAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'code', 'discount_percentage', 'valid_until', 'is_active', 'creator', 'created_at')
+    list_filter = ('is_active', 'valid_until', 'created_at')
+    search_fields = ('code', 'creator__username')
+    ordering = ('created_at',)
+    list_editable = ('is_active',)
