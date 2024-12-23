f411e337eac605e58143448f22618970930ce5d2
measivs <meako.sivsivadze@gmail.com>
2024-12-23 21:18:26 +0400

    Enhance admin interface for Review model
    Added list display, filtering, search fields, ordering, and editable fields to improve the management of reviews in the admin panel. These changes streamline admin tasks and make data organization more efficient.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-
 from reviews.models import Review
-
-
-# Register your models here.
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'user', 'course', 'rating', 'created_at')
+    list_filter = ('rating', 'created_at')
+    search_fields = ('user__username', 'course__title', 'comment')
+    ordering = ('created_at',)
+    list_editable = ('rating',)
